d7ce8c0f236651b8d6d979ff9c1d2348d7039c9e
lencx <cxin1314@gmail.com>
2022-05-25 08:11:27 +0800

    fix: win

--- npm/install.js
+++ npm/install.js
@@ -1,4 +1,20 @@
 #!/usr/bin/env node
 
+const os = require('os');
+const fs = require('fs');
 const { install } = require("./binary");
+const pkgInfo = require("./package.json");
+
 install();
+
+if (os.type() === 'Windows_NT') {
+  pkgInfo.bin = {
+    rsw: 'cmd /C ./bin/rsw.exe',
+  };
+  fs.writeFileSync('./package.json', JSON.stringify(pkgInfo, null, 2));
+} else {
+  pkgInfo.bin = {
+    rsw: './bin/rsw',
+  };
+  fs.writeFileSync('./package.json', JSON.stringify(pkgInfo, null, 2));
+}
